c11ff9e28cf9bcdc89da5c026395f1e58b709f94
GTA6KA <131211404+GTA6KA@users.noreply.github.com>
2023-10-10 13:57:43 +0500

    Bug Fixing

--- Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/Launcher.cs
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/Launcher.cs
@@ -4,6 +4,7 @@ using TMPro;
 using System.Collections.Generic;
 using Photon.Realtime;
 using Unity.VisualScripting;
+using System.Linq;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -11,12 +12,16 @@ public class Launcher : MonoBehaviourPunCallbacks
     private void Awake() => Instance = this;
 
 
-    [SerializeField] TMP_InputField _roomNameInputField;
-    [SerializeField] TMP_Text _errorText;
-    [SerializeField] TMP_Text _roomNameText;
+    [SerializeField] private TMP_InputField _roomNameInputField;
+    [SerializeField] private TMP_Text _errorText;
+    [SerializeField] private TMP_Text _roomNameText;
     [Space]
-    [SerializeField] Transform _roomListContent;
-    [SerializeField] GameObject _roomListItemPregab;
+    [SerializeField] private Transform _roomListContent;
+    [SerializeField] private GameObject _roomListItemPregab;
+    [Space]
+    [SerializeField] private Transform _playerListContent;
+    [SerializeField] private GameObject _playerListItemPregab;
+    [SerializeField] private GameObject _startGameButton;
     private void Start()
     {
         Debug.Log("Connecting to Master");
@@ -27,10 +32,12 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Debug.Log("Connected to Master");
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
     public override void OnJoinedLobby()
     {
         MenuManager.Instance.OpenMenu("title");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
         Debug.Log("Joined Lobby");
     }
     public void CreateRoom()
@@ -42,7 +49,24 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         MenuManager.Instance.OpenMenu("room");
-        _roomNameText.text = PhotonNetwork.CurrentRoom.Name; 
+        _roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach(Transform child in _playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(_playerListItemPregab, _playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+        _startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        _startGameButton.SetActive(PhotonNetwork.IsMasterClient); 
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
@@ -58,14 +82,19 @@ public class Launcher : MonoBehaviourPunCallbacks
 
         for (int i = 0; i < roomList.Count; i++)
         {
+            if (roomList[i].RemovedFromList) continue;
             Instantiate(_roomListItemPregab, _roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(_playerListItemPregab, _playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 
     public void JoinRoom(RoomInfo info)
     {
         PhotonNetwork.JoinRoom(info.Name);
-        MenuManager.Instance.OpenMenu("loading");
+        MenuManager.Instance.OpenMenu("loading"); 
     }
 
     public void LeaveRoom()
@@ -76,5 +105,9 @@ public class Launcher : MonoBehaviourPunCallbacks
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
+    }
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel("GameScene");
     }
 }
--- Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/MenuManager.cs
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/MenuManager.cs
@@ -12,7 +12,7 @@ public class MenuManager : MonoBehaviour
     {
         for (int i = 0; i < menus.Length; i++)
         {
-            if (menus[i].menuName == menuName) OpenMenu(menus[i]);          
+            if (menus[i].menuName == menuName) menus[i].Open();          
             else if (menus[i].open) CloseMenu(menus[i]);  
         }
     }
