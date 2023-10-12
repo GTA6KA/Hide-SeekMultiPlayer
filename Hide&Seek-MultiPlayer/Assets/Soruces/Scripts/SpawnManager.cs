ddab2e2c58bdd43e25707ddade3af214f00ad0e3
GTA6KA <131211404+GTA6KA@users.noreply.github.com>
2023-10-12 08:47:05 +0500

    Added spawn points for hunters

---
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager Instance;
+
+    SpawnPoint[] _spawnPoints;
+    private void Awake()
+    {
+        Instance = this;
+        _spawnPoints = GetComponentsInChildren<SpawnPoint>();
+    }
+
+    public Transform GetSpawnPoint()
+    {
+        return _spawnPoints[Random.Range(0, _spawnPoints.Length)].transform;
+
+    }
+}
---
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/SpawnPoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject _graphics;
+
+    private void Awake()
+    {
+        _graphics.SetActive(false);
+    }
+
+
+
+
+}
