64cacc37a4c3e33cb8f69d0988e990eee8424175
GTA6KA <131211404+GTA6KA@users.noreply.github.com>
2023-10-19 12:29:55 +0500

    Some tests

--- Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
@@ -8,13 +8,22 @@ public class PlayerManager : MonoBehaviour
     private void Awake() => PV = GetComponent<PhotonView>();
     private void Start()
     {
-        if (PV.IsMine) CreateController(); 
+        if (PV.IsMine) CreateController();
+        if (!PV.IsMine) CreateAnotherController();
     }
 
     private void CreateController()
     {
         Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint();
 
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position, spawnPoint.rotation);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position, spawnPoint.rotation);          
+        
+    }
+    private void CreateAnotherController()
+    {
+        Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint();
+
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "BOX"), spawnPoint.position, spawnPoint.rotation);
+
     }
 }
