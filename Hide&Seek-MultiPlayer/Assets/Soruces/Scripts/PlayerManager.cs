603dbd4ef2b9774dec92a734bde4616b8cffa2a2
GTA6KA <131211404+GTA6KA@users.noreply.github.com>
2023-10-23 16:25:46 +0500

    Added random spawn and giving player controller

--- Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView PV;
-    [SerializeField] private GameObject[] _players; 
+    [SerializeField] private GameObject[] _players;
 
     private void Awake() => PV = GetComponent<PhotonView>();
     private void Start()
     {
         if (PV.IsMine) CreateController();
     }
+   
 
     private void CreateController()
     {
         Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint();
-
+        //пиздец он не спавнит рандомно он мне дает одинаковый префаб а..... работает...
         PhotonNetwork.Instantiate(_players[Random.Range(0, _players.Length)].name, spawnPoint.position, spawnPoint.rotation);
         
     }
