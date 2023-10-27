d6758f3ff3c8ef0a62a309c6e11bf1582d8c7a3b
GTA6KA <131211404+GTA6KA@users.noreply.github.com>
2023-10-27 22:31:31 +0500

    Empty

--- Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
+++ Hide&Seek-MultiPlayer/Assets/Soruces/Scripts/PlayerManager.cs
@@ -1,23 +1,21 @@
 using Photon.Pun;
-using System.IO;
 using UnityEngine;
+
 public class PlayerManager : MonoBehaviour
 {
     private PhotonView PV;
-    [SerializeField] private GameObject[] _players;
-
+    [SerializeField] private GameObject[] _hunterPlayers;
     private void Awake() => PV = GetComponent<PhotonView>();
     private void Start()
     {
         if (PV.IsMine) CreateController();
     }
-   
 
     private void CreateController()
     {
         Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint();
-        //пиздец он не спавнит рандомно он мне дает одинаковый префаб а..... работает...
-        PhotonNetwork.Instantiate(_players[Random.Range(0, _players.Length)].name, spawnPoint.position, spawnPoint.rotation);
-        
+
+        PhotonNetwork.Instantiate(_hunterPlayers[Random.Range(0, _hunterPlayers.Length)].name, spawnPoint.position, spawnPoint.rotation);
+
     }
 }
